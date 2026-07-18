a8e56b790d9081a1bcbc7a213979ea0bf384f38d
NoirGoodBoi <phapxover@gmail.com>
2026-07-18 20:32:55 +0700

    Create Icons.lua

---
+++ NhatHub/Icons.lua
@@ -0,0 +1,61 @@
+local IconDB = {
+    -- Menu Pages (Venyx UI dùng tên)
+    ["user"]            = "rbxassetid://10747373176",   -- Player
+    ["sword"]           = "rbxassetid://10734975486",   -- Combat
+    ["target"]          = "rbxassetid://10734977012",   -- Shoot Murder
+    ["map-pin"]         = "rbxassetid://10734886004",   -- Teleport
+    ["eye"]             = "rbxassetid://10723346959",   -- Reveal / Visuals
+    ["dollar-sign"]     = "rbxassetid://10723343958",   -- Farm
+    ["cpu"]             = "rbxassetid://10709813383",   -- Fix lag
+    ["settings"]        = "rbxassetid://10734950309",   -- System
+    ["music"]           = "rbxassetid://10734905958",   -- Music
+
+    -- Nút Ghost (ghostBtn)
+    ["ghost"]           = "rbxassetid://106544313918701",
+
+    -- Nút TK (tkBtn)
+    ["shield"]          = "rbxassetid://10734951847",
+
+    -- Nút KILL ALL (killBtn)
+    ["sword"]           = "rbxassetid://10734975486",
+
+    -- Nút Shoot Murder (shootBtn)
+    ["target"]          = "rbxassetid://10734977012",
+
+    -- Nút Farm (farmBtn)
+    ["dollar-sign"]     = "rbxassetid://10723343958",
+
+    -- Teleport Frame (tpFrame)
+    ["map-pin"]         = "rbxassetid://10734886004",
+    ["home"]            = "rbxassetid://10723407389",
+    ["map"]             = "rbxassetid://10734886202",
+
+    -- Reveal Labels
+    ["sword"]           = "rbxassetid://10734975486",
+    ["shield"]          = "rbxassetid://10734951847",
+
+    -- Aim Panel (aPanel)
+    ["target"]          = "rbxassetid://10734977012",
+
+    -- Fix Lag UI
+    ["cpu"]             = "rbxassetid://10709813383",
+    ["zap"]             = "rbxassetid://130551565616516",
+
+    -- System
+    ["shield"]          = "rbxassetid://10734951847",
+
+    -- Visuals
+    ["eye"]             = "rbxassetid://10723346959",
+
+    -- Music Stop Frame
+    ["music"]           = "rbxassetid://10734905958",
+    ["sword"]           = "rbxassetid://10734975486",
+
+    -- Intro
+    ["intro-1"]         = "rbxassetid://10747373176",
+    ["intro-2"]         = "rbxassetid://14260707709",
+    ["intro-3"]         = "rbxassetid://10723434070",
+
+    -- FPS/Ping Frame
+    ["cpu"]             = "rbxassetid://10709813383",
+}
